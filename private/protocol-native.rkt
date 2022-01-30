d9d6d8533bfa26e5449141e1fc803a5a72ccec2d
Bogdan Popa <bogdan@defn.io>
2022-01-30 18:17:00 +0200

    protocol: add foreign UUID parser

--- private/protocol-native.rkt
+++ private/protocol-native.rkt
@@ -6,10 +6,26 @@
 
 (provide
  (rename-out
+  [parse-uuid UUID]
+  [unparse-uuid un-UUID]
   [parse-bytes Bytes]
   [unparse-bytes un-Bytes]
   [parse-string String]
   [unparse-string un-String]))
+
+(define (expect what len in)
+  (define bs (read-bytes len in))
+  (cond
+    [(eof-object? bs) (make-err in "unexpected EOF while reading ~a" what)]
+    [(< (bytes-length bs) len) (make-err in "input ended before ~a" what)]
+    [else (ok bs)]))
+
+(define (parse-uuid in)
+  (expect 'UUID 16 in))
+
+(define (unparse-uuid out v)
+  (begin0 (ok v)
+    (write-bytes v out)))
 
 (define (parse-bytes in)
   (res-bind
@@ -17,13 +33,7 @@
    (lambda (len)
      (cond
        [(= len -1) (ok 'nil)]
-       [else
-        (define bs
-          (read-bytes len in))
-        (cond
-          [(eof-object? bs) (make-err "unexpected EOF while reading bytes")]
-          [(< (bytes-length bs) len) (make-err in "input ended before bytes")]
-          [else (ok bs)])]))))
+       [else (expect 'bytes len in)]))))
 
 (define (unparse-bytes out bs)
   (cond
@@ -42,13 +52,9 @@
    (lambda (len)
      (cond
        [(= len -1) (ok 'nil)]
-       [else
-        (define str
-          (read-string len in))
-        (cond
-          [(eof-object? str) (make-err in "unexpected EOF while reading string")]
-          [(< (string-length str) len) (make-err in "input ended before string")]
-          [else (ok str)])]))))
+       [else (res-bind
+              (expect 'string len in)
+              (compose1 ok bytes->string/utf-8))]))))
 
 (define (unparse-string out s)
   (cond
