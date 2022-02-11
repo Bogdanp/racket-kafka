b1baf9182bce09cb64e5a43803ee7595f4ab4f71
Bogdan Popa <bogdan@defn.io>
2022-02-11 12:17:27 +0200

    serde: parse batch length

--- private/batch-native.rkt
+++ private/batch-native.rkt
@@ -1,7 +1,9 @@
 #lang racket/base
 
-(require binfmt/runtime/res
+(require binfmt/runtime/parser
+         binfmt/runtime/res
          binfmt/runtime/unparser
+         racket/port
          (prefix-in proto: "batch.bnf"))
 
 (provide
@@ -10,11 +12,14 @@
   [unparse-records un-Records]))
 
 (define (parse-records in)
-  (define b
-    (proto:RecordBatch in))
-  (ok `((Batch_1 . ,b)
-        (Records_1 . ,(for/list ([_ (in-range (cdr (assq 'RecordCount_1 b)))])
-                        (proto:Record in))))))
+  (res-bind
+   (parse-i32be in)
+   (lambda (len)
+     (define batch-in (make-limited-input-port in len #f))
+     (define b (proto:RecordBatch batch-in))
+     (ok `((Batch_1 . ,b)
+           (Records_1 . ,(for/list ([_ (in-range (cdr (assq 'RecordCount_1 b)))])
+                           (proto:Record batch-in))))))))
 
 (define (unparse-records out v)
   (unparse-i32be out (bytes-length v))
