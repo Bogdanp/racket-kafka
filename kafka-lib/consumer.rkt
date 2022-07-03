2f7dfe7c5c54fe37e137d9cb8903dde6a88f11d1
Bogdan Popa <bogdan@defn.io>
2022-07-03 14:26:34 +0300

    consumer: replace never-evt w/ alarm that times out on fetch w/ no nodes
    Also fixed rebalance handling in `join-group!`.

--- kafka-lib/consumer.rkt
+++ kafka-lib/consumer.rkt
@@ -106,45 +106,48 @@
          (values 'rebalance (consumer-topic-partitions c))]
         [else
          (raise e)])))
-   (handle-evt
-    (if (null? node-ids)
-        never-evt
+   (if (null? node-ids)
+       (handle-evt
+        (alarm-evt (+ (current-inexact-milliseconds) timeout))
+        (lambda (_)
+          (values 'records (vector))))
+       (handle-evt
         (make-Fetch-evt
          (get-connection (consumer-client c) node-ids)
          (for/hash ([(topic pids) (in-hash (consumer-topic-partitions c))])
            (values topic (for/list ([(pid offset) (in-hash pids)])
                            (make-TopicPartition #:id pid #:offset offset))))
-         timeout))
-    (lambda (res)
-      (define records
-        (for*/vector ([(topic partitions) (in-hash (FetchResponse-topics res))]
-                      [p (in-list partitions)]
-                      [pid (in-value (FetchResponsePartition-id p))]
-                      [offsets (in-value (hash-ref current-topic-partitions topic))]
-                      [offset (in-value (hash-ref offsets pid))]
-                      [b (in-list (FetchResponsePartition-batches p))]
-                      [r (in-vector (batch-records b))]
-                      #:when (>= (record-offset r) offset))
-          r))
-      (define offsets
-        (for*/fold ([offsets (hash)])
-                   ([(topic partitions) (in-hash (FetchResponse-topics res))]
-                    [p (in-list partitions)]
-                    [b (in-list (FetchResponsePartition-batches p))])
-          (define key (cons topic (FetchResponsePartition-id p)))
-          (define size (batch-size b))
-          (define last-record
-            (and (not (zero? size))
-                 (vector-ref (batch-records b) (sub1 size))))
-          (if last-record
-              (hash-set offsets key (add1 (record-offset last-record)))
-              offsets)))
-      (define topic-partitions
-        (for/hash ([(topic partitions) (in-hash (consumer-topic-partitions c))])
-          (values topic (for/hash ([(pid offset) (in-hash partitions)])
-                          (values pid (hash-ref offsets (cons topic pid) offset))))))
-      (set-consumer-topic-partitions! c topic-partitions)
-      (values 'records records)))))
+         timeout)
+        (lambda (res)
+          (define records
+            (for*/vector ([(topic partitions) (in-hash (FetchResponse-topics res))]
+                          [p (in-list partitions)]
+                          [pid (in-value (FetchResponsePartition-id p))]
+                          [offsets (in-value (hash-ref current-topic-partitions topic))]
+                          [offset (in-value (hash-ref offsets pid))]
+                          [b (in-list (FetchResponsePartition-batches p))]
+                          [r (in-vector (batch-records b))]
+                          #:when (>= (record-offset r) offset))
+              r))
+          (define offsets
+            (for*/fold ([offsets (hash)])
+                       ([(topic partitions) (in-hash (FetchResponse-topics res))]
+                        [p (in-list partitions)]
+                        [b (in-list (FetchResponsePartition-batches p))])
+              (define key (cons topic (FetchResponsePartition-id p)))
+              (define size (batch-size b))
+              (define last-record
+                (and (not (zero? size))
+                     (vector-ref (batch-records b) (sub1 size))))
+              (if last-record
+                  (hash-set offsets key (add1 (record-offset last-record)))
+                  offsets)))
+          (define topic-partitions
+            (for/hash ([(topic partitions) (in-hash (consumer-topic-partitions c))])
+              (values topic (for/hash ([(pid offset) (in-hash partitions)])
+                              (values pid (hash-ref offsets (cons topic pid) offset))))))
+          (set-consumer-topic-partitions! c topic-partitions)
+          (values 'records records))))))
 
 (define (consumer-commit c)
   (with-handlers* ([coordinator-error?
@@ -226,14 +229,14 @@
       (sync thd)))))
 
 (define (join-group! c)
-  (with-handlers* ([exn:fail:kafka:server?
-                    (λ (e)
-                      (case (exn:fail:kafka:server-code e)
-                        [(unknown-member-id rebalance-in-progress)
-                         (log-kafka-debug "retrying group join due to rebalance")
-                         (join-group! c)]
-                        [else
-                         (raise e)]))])
+  (with-handlers* ([(make-error-code?
+                     (λ (sym)
+                       (case sym
+                         [(unknown-member-id rebalance-in-progress) #t]
+                         [else #f])))
+                    (λ (_e)
+                      (log-kafka-debug "retrying group join due to rebalance")
+                      (join-group! c))])
     (do-join-group! c)))
 
 (define (do-join-group! c)
