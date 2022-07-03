d57f726bdd7425e912e5d4800e48200875d488a0
Bogdan Popa <bogdan@defn.io>
2022-07-03 08:12:38 +0300

    build: require binfmt 0.2+

--- kafka-lib/info.rkt
+++ kafka-lib/info.rkt
@@ -3,7 +3,7 @@
 (define collection "kafka")
 (define version "0.1")
 (define deps '("base"
-               "binfmt"
+               ("binfmt" #:version "0.2")
                "box-extra-lib"
                "sasl-lib"))
 (define build-deps '("rackunit-lib"))
