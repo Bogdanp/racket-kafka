39a25759a312b535666cb7f247b6ac00cb76e4b8
Bogdan Popa <bogdan@defn.io>
2022-10-02 19:44:01 +0300

    build: bump minor version

--- kafka-lib/info.rkt
+++ kafka-lib/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define collection "kafka")
-(define version "0.1")
+(define version "0.2")
 (define deps '("base"
                ("binfmt" #:version "0.2")
                "sasl-lib"))
