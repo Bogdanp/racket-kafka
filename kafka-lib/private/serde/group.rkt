40caa9a089aa5bacd497d8b3bb223c340278936d
Bogdan Popa <bogdan@defn.io>
2022-10-16 12:12:27 +0300

    core,serde: add group parsing helpers

--- kafka-lib/private/serde/group.rkt
+++ kafka-lib/private/serde/group.rkt
@@ -1,8 +1,16 @@
 #lang racket/base
 
 (require racket/contract
+         racket/port
+         (prefix-in consumer: "../protocol-consumer.bnf")
          "authorized-operation.rkt"
          "core.rkt")
+
+(provide
+ (contract-out
+  [Group-topics (-> Group? (listof string?))]
+  [GroupMember-topics (-> GroupMember? (listof string?))]
+  [GroupMember-assignments (-> GroupMember? (hash/c string? (listof exact-integer?)))]))
 
 (define-record GroupMember
   ([id string?]
@@ -19,3 +27,29 @@
    [(protocol-data #f) (or/c #f string?)]
    [(members null) (listof GroupMember?)]
    [(authorized-operations null) (listof authorized-operation/c)]))
+
+(define (Group-topics g)
+  (case (Group-protocol-type g)
+    [("consumer")
+     (for*/fold ([topics (hash)] #:result (sort (hash-keys topics) string<?))
+                ([m (in-list (Group-members g))]
+                 [t (in-list (GroupMember-topics m))])
+       (hash-set topics t #t))]
+    [else null]))
+
+(define (GroupMember-topics m)
+  (with-handlers ([exn:fail? (λ (_) null)])
+    (define res
+      (call-with-input-bytes (GroupMember-metadata m)
+        consumer:MemberMetadata))
+    (ref 'TopicName_1 res)))
+
+(define (GroupMember-assignments m)
+  (with-handlers ([exn:fail? (λ (_) (hash))])
+    (define res
+      (call-with-input-bytes (GroupMember-assignment m)
+        consumer:MemberAssignment))
+    (for/hash ([topic (in-list (ref 'Assignment_1 res))])
+      (values
+       (ref 'TopicName_1 topic)
+       (ref 'PartitionID_1 topic)))))
