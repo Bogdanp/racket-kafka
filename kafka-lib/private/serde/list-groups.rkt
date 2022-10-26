5494c958b1507241348a1d7d96d0f9a92eaf3ae5
Bogdan Popa <bogdan@defn.io>
2022-10-26 11:09:17 +0300

    serde: add list-groups v1, v2, and v3

--- kafka-lib/private/serde/list-groups.rkt
+++ kafka-lib/private/serde/list-groups.rkt
@@ -8,8 +8,32 @@
   #:version 0
   #:response proto:ListGroupsResponseV0
   (lambda () #"")
+  (dec-list-groups-response-v012 'ListGroupsGroupV0_1)
+
+  #:version 1
+  #:response proto:ListGroupsResponseV1
+  (lambda () #"")
+  (dec-list-groups-response-v012 'ListGroupsGroupV1_1)
+
+  #:version 2
+  #:response proto:ListGroupsResponseV2
+  (lambda () #"")
+  (dec-list-groups-response-v012 'ListGroupsGroupV2_1)
+
+  #:version 3
+  #:flexible #t
+  #:response proto:ListGroupsResponseV3
+  (lambda ()
+    (with-output-bytes
+      (proto:un-ListGroupsRequestV3 (hash))))
   (lambda (res)
-    (for/list ([g (in-list (ref 'ListGroupsGroupV0_1 res))])
+    (for/list ([g (in-list (ref 'ListGroupsGroupV3_1 res))])
       (make-Group
-       #:id (ref 'GroupID_1 g)
-       #:protocol-type (ref 'ProtocolType_1 g)))))
+       #:id (ref 'CompactGroupID_1 g)
+       #:protocol-type (ref 'CompactProtocolType_1 g)))))
+
+(define ((dec-list-groups-response-v012 list-id) res)
+  (for/list ([g (in-list (ref list-id res))])
+    (make-Group
+     #:id (ref 'GroupID_1 g)
+     #:protocol-type (ref 'ProtocolType_1 g))))
