12e85bd759d5769db2a0f0aa28c37088b2428b1e
Bogdan Popa <bogdan@defn.io>
2022-10-31 08:07:14 +0200

    iterator: use immutable offset hash, avoid making rpcs for exact offsets

--- kafka-lib/iterator.rkt
+++ kafka-lib/iterator.rkt
@@ -57,7 +57,10 @@
 
 (define (get-records it #:max-bytes [max-bytes (* 1 1024 1024)])
   (match-define (topic-iterator c topic metadata offsets) it)
-  (get-records* c topic metadata offsets max-bytes))
+  (define-values (records updated-offsets)
+    (get-records* c topic metadata offsets max-bytes))
+  (begin0 records
+    (set-topic-iterator-offsets! it updated-offsets)))
 
 
 ;; help ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -68,30 +71,34 @@
    (Metadata-topics (reload-metadata c))))
 
 (define (get-offsets c topic-name offset)
-  (define offsets (make-hasheqv))
-  (define nodes-by-t&p (collect-nodes-by-topic&pid (client-metadata c) (list topic-name)))
-  (define pids-by-node
-    (for/fold ([nodes (hasheqv)])
-              ([(t&p node-id) (in-hash nodes-by-t&p)])
-      (define pid (cdr t&p))
-      (hash-update nodes node-id (λ (pids) (cons pid pids)) null)))
-  (define promises
-    (for/list ([(node-id pids) (in-hash pids-by-node)])
-      (delay/thread
-       (define conn (get-node-connection c node-id))
-       (sync (make-ListOffsets-evt conn (hash topic-name (for/hasheqv ([pid (in-list pids)])
-                                                           (values pid offset))))))))
-  (begin0 offsets
-    (for* ([promise (in-list promises)]
-           [res (in-value (force promise))]
-           [parts (in-hash-values res)]
-           [part (in-list parts)])
-      (define pid (PartitionOffset-id part))
-      (unless (zero? (PartitionOffset-error-code part))
-        (raise-server-error (PartitionOffset-error-code part)))
-      (hash-set! offsets pid (if (symbol? offset)
-                                 (PartitionOffset-offset part)
-                                 offset)))))
+  (case offset
+    [(earliest latest)
+     (define nodes-by-t&p (collect-nodes-by-topic&pid (client-metadata c) (list topic-name)))
+     (define pids-by-node
+       (for/fold ([nodes (hasheqv)])
+                 ([(t&p node-id) (in-hash nodes-by-t&p)])
+         (define pid (cdr t&p))
+         (hash-update nodes node-id (λ (pids) (cons pid pids)) null)))
+     (define promises
+       (for/list ([(node-id pids) (in-hash pids-by-node)])
+         (delay/thread
+          (define conn (get-node-connection c node-id))
+          (sync (make-ListOffsets-evt conn (hash topic-name (for/hasheqv ([pid (in-list pids)])
+                                                              (values pid offset))))))))
+     (for*/hasheqv ([promise (in-list promises)]
+                    [res (in-value (force promise))]
+                    [parts (in-hash-values res)]
+                    [part (in-list parts)])
+       (define pid (PartitionOffset-id part))
+       (unless (zero? (PartitionOffset-error-code part))
+         (raise-server-error (PartitionOffset-error-code part)))
+       (values pid (PartitionOffset-offset part)))]
+    [else
+     (unless (exact-nonnegative-integer? offset)
+       (raise-argument-error 'get-offsets "(or/c 'earliest 'latest exact-nonnegative-integer?)" offset))
+     (for*/hasheqv ([part (TopicMetadata-partitions (get-topic c topic-name))])
+       (define pid (PartitionMetadata-id part))
+       (values pid offset))]))
 
 (define (get-records* c topic-name metadata offsets max-bytes)
   (define partitions-by-node
@@ -129,17 +136,20 @@
                   [pid (in-value (FetchResponsePartition-id part))]
                   [batch (in-list (FetchResponsePartition-batches part))]
                   [record (in-vector (batch-records batch))]
-                  #:when (>= (record-offset record) (hash-ref offsets pid 0)))
+                  #:when (>= (record-offset record)
+                             (hash-ref offsets pid 0)))
       (begin0 record
         (set-record-partition-id! record pid))))
-  (begin0 records
-    (for* ([response (in-list responses)]
-           #:when response
-           [parts (in-hash-values (FetchResponse-topics response))]
-           [part (in-list parts)]
-           [b (in-list (FetchResponsePartition-batches part))])
+  (define updated-offsets
+    (for*/fold ([offsets offsets])
+               ([response (in-list responses)]
+                #:when response
+                [parts (in-hash-values (FetchResponse-topics response))]
+                [part (in-list parts)]
+                [b (in-list (FetchResponsePartition-batches part))]
+                #:unless (zero? (batch-size b)))
       (define pid (FetchResponsePartition-id part))
       (define size (batch-size b))
-      (unless (zero? size)
-        (define last-record (vector-ref (batch-records b) (sub1 size)))
-        (hash-set! offsets pid (add1 (record-offset last-record)))))))
+      (define last-record (vector-ref (batch-records b) (sub1 size)))
+      (hash-set offsets pid (add1 (record-offset last-record)))))
+  (values records updated-offsets))
