d1047f348839f354b42d907798e1ecb1dfdc84dc
Bogdan Popa <bogdan@defn.io>
2023-09-18 20:26:04 +0300

    build: require binfmt-lib instead of binfmt

--- kafka-lib/info.rkt
+++ kafka-lib/info.rkt
@@ -4,7 +4,7 @@
 (define version "0.10")
 (define collection "kafka")
 (define deps '("base"
-               ["binfmt" #:version "0.4"]
+               ["binfmt-lib" #:version "0.5"]
                ["libzstd" #:version "1.5.5"]
                ["lz4-lib" #:version "1.4.1"]
                "net-lib"
